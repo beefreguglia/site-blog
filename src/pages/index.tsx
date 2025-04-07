eefbcf0d3577d4277b88dce43658003f89058c74
Bernardo <beefreguglia@gmail.com>
2025-04-07 16:09:37 -0300

    feature: create header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
+import { Header } from "@/components/header";
+import { Geist, Geist_Mono } from 'next/font/google'
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+})
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+})
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <div className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_2px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}>
+      <Header />
+    </div>
   );
 }
