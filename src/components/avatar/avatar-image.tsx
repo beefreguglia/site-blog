c459854c67e8e214d88a2b093e4e5c42da16af54
Bernardo <beefreguglia@gmail.com>
2025-04-14 07:31:19 -0300

    feature: update avatar image component

--- src/components/avatar/avatar-image.tsx
+++ src/components/avatar/avatar-image.tsx
@@ -1,9 +1,35 @@
+import { cn } from "@/lib/utils";
 import Image, { ImageProps } from "next/image";
 
-type AvatarImageProps = ImageProps;
+type AvatarSize = "xs" | "sm";
 
-export function AvatarImage({ src, alt, width = 40, height = 40, ...rest }: AvatarImageProps) {
+type AvatarImageProps = Omit<ImageProps, "height" | "width"> & {
+  size?: AvatarSize;
+};
+
+const avatarSize = {
+  xs: "size-5",
+  sm: "size-9",
+};
+
+export function AvatarImage({
+  src,
+  alt,
+  size = "xs",
+  ...rest
+}: AvatarImageProps) {
   return (
-    <Image src={src} alt={alt} width={width} height={height} {...rest} />
-  )
+    <div>
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        className={cn(
+          "relative overflow-hidden rounded-full border-blue-200 border",
+          avatarSize[size]
+        )}
+        {...rest}
+      />
+    </div>
+  );
 }
