ad46035e58f47fd773ae4b24b50d4798a2c7093f
Bernardo <beefreguglia@gmail.com>
2025-04-28 13:29:41 -0300

    feature: add SEO in blog post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { PostPage } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
 import { notFound } from "next/navigation";
@@ -7,6 +8,27 @@ type BlogPostPageProps = {
     slug: string;
   }>;
 };
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    return {};
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: "index, follow",
+    openGraph: {
+      images: [post.image],
+    },
+  };
+}
 
 export const revalidate = 60;
 
